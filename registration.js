4b41714eba9b804b9028649d2258e14197973bb6
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-12-24 09:45:26 +0545

    create registration.js file

---
+++ registration.js
@@ -0,0 +1,32 @@
+console.log("This is registration page");
+
+const registrationForm = document.getElementById("registration-form");
+
+console.log(registrationForm);
+
+function getSelected(select) {
+  let selectedValues = [];
+  for (optionIndex in select.options) {
+    if (select.options[optionIndex].selected) {
+      selectedValues.push(select.options[optionIndex].value);
+    }
+  }
+  return selectedValues;
+}
+
+registrationForm.onsubmit = function (event) {
+  event.preventDefault();
+  console.log("I am triggered when submit is pressed");
+  let name = document.getElementById("name").value;
+  let interests = getSelected(document.getElementById("interests"));
+  let dob = document.getElementById("dob").value;
+  let darkMode = document.getElementById("dark-mode").checked;
+  const data = {
+    name: name,
+    interests: interests,
+    dob: dob,
+    darkMode: darkMode,
+  };
+  console.log(data);
+  localStorage.setItem("userInfo", JSON.stringify(data));
+};
