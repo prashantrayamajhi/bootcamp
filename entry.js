f1019c5888e22d606854c6962cd2c7ec3bb91e58
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-12-26 08:52:25 +0545

    initialize electron

---
+++ entry.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    title: "Bootcamp",
+    width: 800,
+    height: 600,
+  });
+
+  // mainWindow.loadURL("")
+  mainWindow.loadFile("home.html");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  mainWindow === null;
+});
